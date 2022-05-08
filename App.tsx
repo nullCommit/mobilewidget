b3dcb9b8d996b3d068d96a8e83666922cd2e77de
Allan <allangeek@gmail.com>
2022-05-08 16:59:26 -0300

    Feature: add a bottom modal

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 import { theme } from './src/theme';
 import { useCallback, useEffect, useState } from 'react';
 
@@ -52,8 +53,8 @@ export default function App() {
       style={{ flex: 1, backgroundColor: theme.colors.background }}
       onLayout={onLayoutRootView}
     >
-      <Widget />
       <StatusBar style='light' backgroundColor='transparent' translucent />
+      <Widget />
     </View>
   );
 }
--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
 
-export function Widget() {
+function Widget() {
+  const bottonSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottonSheetRef.current?.expand();
+  }
+
   return (
     <>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
         <ChatTeardropDots
           size={24}
           weight='bold'
           color={theme.colors.text_on_brand_color}
         />
       </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottonSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      ></BottomSheet>
     </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
