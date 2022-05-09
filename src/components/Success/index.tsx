aba34ea01fa79a5b90db04663565571dfbc0be9a
Allan <allangeek@gmail.com>
2022-05-09 11:05:37 -0300

    Feature: add success component

--- src/components/Success/index.tsx
+++ src/components/Success/index.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 
 import successImg from '../../assets/success.png';
+import { Copyright } from '../Copyright';
 
 import { styles } from './styles';
 
 export function Success() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Image style={styles.image} source={successImg} />
+
+      <Text style={styles.title}>Agradecemos o feedback</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonTitle}>Quero enviar outro</Text>
+      </TouchableOpacity>
+
+      <Copyright />
     </View>
   );
 }
